b7b4e097c74647115c35e0009446c4c78b3de184
william-j-m-m <w.j.m.mendez@gmail.com>
2024-02-02 12:42:45 +0000

    Followed the correct format for the file. Reading in data for exercsise 2.

---
+++ src/Exercise_2/Spheroid.java
@@ -0,0 +1,12 @@
+package Exercise_2;
+import java.util.Scanner;
+
+public class Spheroid {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Equatorial and Polar radii (in km): ");
+        double equatorial = input.nextDouble();
+        double polarRadii = input.nextDouble();
+
+    }
+}
