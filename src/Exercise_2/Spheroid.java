89ea7a6b4219e987c4c5bafe0f7dc53e2ecd2800
william-j-m-m <w.j.m.mendez@gmail.com>
2024-02-02 13:01:55 +0000

    Finished exercise 2.

--- src/Exercise_2/Spheroid.java
+++ src/Exercise_2/Spheroid.java
@@ -1,5 +1,5 @@
-package Exercise_2;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Spheroid {
     public static void main(String[] args) {
@@ -8,5 +8,10 @@ public class Spheroid {
         double equatorial = input.nextDouble();
         double polarRadii = input.nextDouble();
 
+        double eccentricity = Math.sqrt(1 - (Math.pow(polarRadii, 2) / (Math.pow(equatorial, 2))));
+        double volume = (4 * Math.PI * Math.pow(equatorial, 2) * polarRadii) / 3;
+
+        System.out.printf("Eccentricity = %.3f\n", eccentricity);
+        System.out.printf("Volume = %g cubic km\n", volume);
     }
 }
