46b575e4805abf9cae5ecfd3e14452d0c39db7be
william-j-m-m <w.j.m.mendez@gmail.com>
2024-02-19 15:08:21 +0000

    basic format for exercise 8

---
+++ src/exercise8/Dataset.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Dataset {
+    private ArrayList<Double> data = new ArrayList<Double>();
+
+    public Dataset(String filename) {
+
+    }
+
+    public int size() {
+
+    }
+
+    public double meanValue() {
+
+    }
+
+    public class Dataset {
+
+    }
+
+}
