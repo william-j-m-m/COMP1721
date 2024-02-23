05158184f478706a6aad81150a13c342e3ade1c4
william-j-m-m <w.j.m.mendez@gmail.com>
2024-02-23 12:24:00 +0000

    finished exercise 8

--- src/exercise8/Dataset.java
+++ src/exercise8/Dataset.java
@@ -1,22 +1,33 @@
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Dataset {
     private ArrayList<Double> data = new ArrayList<Double>();
 
-    public Dataset(String filename) {
+    public Dataset(String filename) throws IOException {
 
+        try (Scanner input = new Scanner(Paths.get(filename))) {
+            while (input.hasNextDouble()) {
+                data.add(input.nextDouble());
+            }
+        }
     }
 
     public int size() {
-
+        return data.size();
     }
 
-    public double meanValue() {
+    public double meanValue() throws ArithmeticException{
+        if (size() == 0) {
+            throw new ArithmeticException("Error: No Values Present");
+        }
+        double sum = 0;
+        for (double value : data) {
+            sum += value;
+        }
 
+        return sum / size();
     }
-
-    public class Dataset {
-
-    }
-
 }
